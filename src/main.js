4396e4011e21768243a92c5803fe42f0dd4bcbff
Ray Perry <rmperry09@gmail.com>
2022-06-29 14:07:15 -0500

    NOJIRA | Add some puzzles. | [Ray]

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import kaboom from 'kaboom';
 
+const { puzzles: { puzzle01 } } = require('./puzzles.json');
+
 const gameOptions = {
   width: 640,
   height: 480,
@@ -10,8 +12,34 @@ const gameOptions = {
 
 kaboom(gameOptions);
 
+loadSprite("serrano", "assets/sprites/serrano.png");
+
+const { rows, columns } = puzzle01;
+
+const NUMBER_OFFSET = 20;
+const Y_OFFSET = 100;
+const X_OFFSET = 160;
+
 add([
-  text("Super Spicy Squad! Let's sauce 'em!"),
-  pos(40, 400),
-  scale(2)
+  sprite("serrano"),
+  pos(X_OFFSET, Y_OFFSET),
+  scale(20)
 ]);
+
+const allRowEntities = rows.map((row, rowIndex) => {
+  row.reverse();
+  return row.map((number, numberIndex) => add([
+    text(number),
+    pos(45 - (numberIndex * NUMBER_OFFSET) + X_OFFSET - 60, (rowIndex * NUMBER_OFFSET) + Y_OFFSET),
+    scale(2)
+  ]))
+});
+
+const allColumnEntities = columns.map((column, columnIndex) => {
+  column.reverse();
+  return column.map((number, numberIndex) => add([
+    text(number),
+    pos((columnIndex * NUMBER_OFFSET) + X_OFFSET, (60 - (numberIndex * NUMBER_OFFSET)) + NUMBER_OFFSET),
+    scale(2)
+  ]))
+});  
